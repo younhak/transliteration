0cf290e75df92f3d137d7d02883555e4b920dc37
younak <younhak.oh@gmail.com>
2018-12-05 22:12:14 +0900

    update split_kr: split korean word into chosung, jungsung, jongsung

---
+++ kor2eng/kor2eng.py
@@ -0,0 +1,24 @@
+import re
+
+BASE_CODE, CHOSUNG, JUNGSUNG = 44032, 588, 28
+
+# 00 - 18
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# 00 - 20
+JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+
+# 00 - 27 + 1 (1 is None)
+JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+def split_kor(kor_word):
+    res = []
+    for c in list(kor_word.strip()):
+        if '가' <= c <= '힣':
+            chosung = (ord(c) - ord('가')) // CHOSUNG
+            jungsung = ((ord(c) - ord('가')) - CHOSUNG * chosung) // JUNGSUNG
+            jongsung = (ord(c) - ord('가')) - CHOSUNG * chosung - JUNGSUNG * jungsung
+            res.append([CHOSUNG_LIST[chosung], JUNGSUNG_LIST[jungsung], JONGSUNG_LIST[jongsung]])
+        else:
+            res.append([c])
+    return res
